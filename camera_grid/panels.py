1171e28fa618eba638b7cc0f3656ddda1dbfa24b
Nadir <n4dirp@gmail.com>
2026-07-22 00:00:20 -0300

    Flatten panel hierarchy in Camera Grid UI

--- camera_grid/panels.py
+++ camera_grid/panels.py
@@ -17,73 +17,71 @@ class CAMGRID_PT_grid_popup(Panel):
         prefs = context.preferences.addons.get(__package__).preferences
         props = context.scene.camgrid_props
 
-        cam_header, cam_body = layout.panel("CAMGRID_PT_camera_grid", default_closed=False)
-        cam_header.label(text="Camera Grid")
+        layout.label(text="Camera Grid")
 
-        if cam_body:
-            header, body = cam_body.panel("CAMGRID_PT_camera_grid_filter_list", default_closed=True)
-            header.label(text="Filter")
-            if body:
+        header, body = layout.panel("CAMGRID_PT_camera_grid_filter_list", default_closed=True)
+        header.label(text="Filter")
+        if body:
+            row = body.row(align=True)
+            row.prop(props, "source_collection", text="")
+            row.prop(prefs.settings, "filter_camera_collections", text="", icon="VIEW_CAMERA")
+            body.prop(prefs.settings, "show_hidden", text="Show Hidden Cameras")
+
+        header, body = layout.panel("CAMGRID_PT_camera_grid_ui", default_closed=False)
+        header.label(text="Interface")
+        if body:
+            col = body.column()
+            col.label(text="Alignment")
+            col.row().prop(prefs.settings, "alignment", expand=True)
+
+            col = body.column()
+            col.label(text="Display Mode")
+            col.prop(prefs.settings, "display_type", text="Display Mode", expand=True)
+
+            col = body.column()
+            col.label(text="Appearance")
+
+            sub = col.column(align=True)
+            if prefs.settings.display_type == "THUMBNAILS":
+                sub.prop(prefs.settings, "preview_size", text="Size")
+                sub.prop(prefs.settings, "preview_max_rows", text="Max Rows")
+                sub.prop(prefs.settings, "preview_max_columns", text="Max Columns")
+            elif prefs.settings.display_type == "DOTS":
+                sub.prop(prefs.settings, "dots_max_rows", text="Max Rows")
+                sub.prop(prefs.settings, "dots_max_columns", text="Max Columns")
+            else:
+                sub.prop(prefs.settings, "tile_size", text="Size")
+                sub.prop(prefs.settings, "max_rows", text="Max Rows")
+                sub.prop(prefs.settings, "max_columns", text="Max Columns")
+
+            if prefs.settings.display_type == "THUMBNAILS":
                 row = body.row(align=True)
-                row.prop(props, "source_collection", text="")
-                row.prop(prefs.settings, "filter_camera_collections", text="", icon="VIEW_CAMERA")
-                body.prop(prefs.settings, "show_hidden", text="Show Hidden Cameras")
+                row.prop(prefs.settings, "preview_disable_overlays", text="Hide Overlays")
+                row.prop(prefs.settings, "preview_show_names", text="Show Names")
 
-            header, body = cam_body.panel("CAMGRID_PT_camera_grid_ui", default_closed=False)
-            header.label(text="Interface")
-            if body:
-                col = body.column()
-                col.label(text="Alignment")
-                col.row().prop(prefs.settings, "alignment", expand=True)
+            body.separator()
+            col = body.column(align=True)
+            col.label(text="Info Text")
+            row = col.row(align=True)
+            row.prop(prefs.settings, "show_active_camera_name", text="Name")
+            row.prop(prefs.settings, "show_camera_settings", text="Lens")
+            row.prop(prefs.settings, "show_camera_count", text="Count")
 
-                col = body.column()
-                col.label(text="Display Mode")
-                col.prop(prefs.settings, "display_type", text="Display Mode", expand=True)
+        header, body = layout.panel("CAMGRID_PT_camera_grid_interaction", default_closed=False)
+        header.label(text="Options")
 
-                col = body.column()
-                col.label(text="Appearance")
+        if body:
+            col = body.column()
+            col.label(text="Scroll Wheel")
+            col.row().prop(prefs.settings, "wheel_mode", text="Mouse Wheel", expand=True)
 
-                sub = col.column(align=True)
-                if prefs.settings.display_type == "THUMBNAILS":
-                    sub.prop(prefs.settings, "preview_size", text="Size")
-                    sub.prop(prefs.settings, "preview_max_rows", text="Max Rows")
-                    sub.prop(prefs.settings, "preview_max_columns", text="Max Columns")
-                elif prefs.settings.display_type == "DOTS":
-                    sub.prop(prefs.settings, "dots_max_rows", text="Max Rows")
-                    sub.prop(prefs.settings, "dots_max_columns", text="Max Columns")
-                else:
-                    sub.prop(prefs.settings, "tile_size", text="Size")
-                    sub.prop(prefs.settings, "max_rows", text="Max Rows")
-                    sub.prop(prefs.settings, "max_columns", text="Max Columns")
+            col = body.column()
+            col.label(text="On Switch")
+            col.prop(prefs.settings, "on_switch_action", text="")
 
-                if prefs.settings.display_type == "THUMBNAILS":
-                    row = body.row(align=True)
-                    row.prop(prefs.settings, "preview_disable_overlays", text="Hide Overlays")
-                    row.prop(prefs.settings, "preview_show_names", text="Show Names")
-
-                body.separator()
-                col = body.column(align=True)
-                col.label(text="Info Text")
-                row = col.row(align=True)
-                row.prop(prefs.settings, "show_active_camera_name", text="Name")
-                row.prop(prefs.settings, "show_camera_settings", text="Lens")
-                row.prop(prefs.settings, "show_camera_count", text="Count")
-
-            header, body = cam_body.panel("CAMGRID_PT_camera_grid_interaction", default_closed=False)
-            header.label(text="Options")
-
-            if body:
-                col = body.column()
-                col.label(text="Scroll Wheel")
-                col.row().prop(prefs.settings, "wheel_mode", text="Mouse Wheel", expand=True)
-
-                col = body.column()
-                col.label(text="On Switch")
-                col.prop(prefs.settings, "on_switch_action", text="")
-
-                col = body.column()
-                col.prop(prefs.settings, "cycle_cameras", text="Loop Through Cameras")
-                col.prop(prefs.settings, "close_on_esc", text="Exit with Escape Key")
+            col = body.column()
+            col.prop(prefs.settings, "cycle_cameras", text="Loop Through Cameras")
+            col.prop(prefs.settings, "close_on_esc", text="Exit with Escape Key")
 
         header, body = layout.panel("CAMGRID_PT_frame_camera", default_closed=True)
         header.label(text="Frame Camera")
